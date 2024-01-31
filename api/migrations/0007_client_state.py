50b948fef4ace0220024582f2a2527e2b61c39e3
IsaacCRS17 <crs170919@gmail.com>
2024-01-31 10:21:57 -0500

    Fix: migrations

--- api/migrations/0007_client_state.py
+++ api/migrations/0007_client_state.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0002_category_product'),
+        ('api', '0006_alter_category_color'),
     ]
 
     operations = [
