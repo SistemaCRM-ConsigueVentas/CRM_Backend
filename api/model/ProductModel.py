db10801eae0ea0a3f2a77384eb2bc6b3b844efe2
LuisGH18 <lguanilo270@gmail.com>
2024-03-19 13:22:19 -0500

    Modified: CategoryModel-ProductModel

--- api/model/ProductModel.py
+++ api/model/ProductModel.py
@@ -3,14 +3,18 @@ from api.model.CategoryModel import Category
 from api.enums.ProductStatusEnums import ProductStatusEnums
 
 class Product(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField()
-    price = models.FloatField()
-    stock = models.IntegerField()
-    security_stock = models.IntegerField()
+    id = models.AutoField(primary_key=True)
     barcode = models.CharField(max_length=100)
-    state = models.IntegerField(choices=[(e.value, e.name) for e in ProductStatusEnums])
-    image_url = models.CharField(max_length=300)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    brand = models.CharField(max_length=50)
+    stock = models.IntegerField()
+    stock_security = models.IntegerField()
+    price = models.FloatField()
+    rating = models.CharField(max_length=20)
+    image_url = models.CharField(max_length=150)
+    status = models.IntegerField(choices=[(e.value, e.name) for e in ProductStatusEnums])
+    
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
