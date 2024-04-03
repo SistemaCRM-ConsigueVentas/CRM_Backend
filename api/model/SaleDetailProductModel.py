6eba2ef551111c3df3ac8f20715af67e72c226b2
Kenneth Molina <kenneth-molina899@outlook.es>
2024-04-03 13:05:54 -0500

    CreateCustomerAndCreateSales2

--- api/model/SaleDetailProductModel.py
+++ api/model/SaleDetailProductModel.py
@@ -12,3 +12,6 @@ class SaleDetailProduct(models.Model):
     total_item_amount = models.DecimalField(max_digits=10, decimal_places=2)
     created_at=models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.product_id.name
+
