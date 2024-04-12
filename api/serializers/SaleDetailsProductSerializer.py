1386647a07013c48fa43051e6581513b0812aa22
Claudio Jara <holasoyarturo123.com@gmail.com>
2024-04-12 13:00:38 -0500

    Fix: update and conflict with migrations

--- api/serializers/SaleDetailsProductSerializer.py
+++ api/serializers/SaleDetailsProductSerializer.py
@@ -5,7 +5,7 @@ from decimal import Decimal
 
 class SaleDetailsProductSerializer(serializers.ModelSerializer):
     
-    sale = SaleSerializer(read_only=True)
+    sale_obj = SaleSerializer(source='sale', read_only=True)
     sale = serializers.PrimaryKeyRelatedField(queryset=Sale.objects.all(), write_only=True)
     
     tax = serializers.DecimalField(max_digits=5, decimal_places=2, read_only=True)
@@ -13,23 +13,21 @@ class SaleDetailsProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = SaleDetailsProduct
-        fields = ['id', 'quantity', 'unit_price', 'discount', 'tax', 'total_item_amount', 'created_at', 'sale', 'product']
-
+        fields = ['id', 'quantity', 'unit_price', 'discount', 'tax', 'total_item_amount', 'created_at', 'sale_obj', 'product', 'sale']
 
     def create(self, validated_data):
         sale = validated_data.pop('sale')
         total_item_amount = (validated_data['quantity'] * validated_data['unit_price']) - validated_data['discount']
-        tax_rate = Decimal(SaleDetailsProduct.TAX_RATE)
-        validated_data['total_item_amount'] = total_item_amount + (total_item_amount * tax_rate)
+        validated_data['total_item_amount'] = total_item_amount + (total_item_amount * Decimal(SaleDetailsProduct.TAX_RATE))
         validated_data['sale'] = sale
         return super().create(validated_data)
-
 
     def update(self, instance, validated_data):
         # Actualiza los datos del objeto
         instance.quantity = validated_data.get('quantity', instance.quantity)
         instance.unit_price = validated_data.get('unit_price', instance.unit_price)
         instance.discount = validated_data.get('discount', instance.discount)
+        instance.sale = validated_data.get('sale', instance.sale)
         
         # Recalcula el total_item_amount automáticamente
         total_item_amount = (instance.quantity * instance.unit_price) - instance.discount
