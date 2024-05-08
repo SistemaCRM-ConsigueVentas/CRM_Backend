9c9a618cfb26418c8eded2ba7ca9e81b0a562d95
Claudio Jara <holasoyarturo123.com@gmail.com>
2024-05-08 11:57:26 -0500

    Update: sale fields (SaleStatus, user_id, note)

---
+++ api/enums/SaleStatusEnums.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class SaleStatusEnums(Enum):
+    PENDIENTE = 0
+    EN_PROCESO = 1
+    COMPLETADO = 2
--- api/model/SaleModel.py
+++ api/model/SaleModel.py
@@ -1,15 +1,20 @@
 from django.db import models
 from api.model.CustomerModel import Customer
+from api.model.UserModel import User
 from api.enums.PaymentMethodEnums import PaymentMethodEnums
+from api.enums.SaleStatusEnums import SaleStatusEnums
 
 class Sale(models.Model):
     saleID = models.AutoField(primary_key=True)
     date = models.DateField()
     total = models.DecimalField(max_digits=10, decimal_places=2)
     paymentType = models.CharField(max_length=1, choices=[(e.value, e.name) for e in PaymentMethodEnums])
+    saleStatus = models.CharField(max_length=1, choices=[(e.value, e.name) for e in SaleStatusEnums])
+    note = models.TextField(null=True, blank=True)
     
     #clave foránea con cliente
     customer = models.ForeignKey(Customer, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
     
     #auditoría
     created_at = models.DateTimeField(auto_now_add=True)#Fecha de creacion
