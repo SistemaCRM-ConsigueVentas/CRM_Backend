046424efc4374de89211d8bf016f60fc37d4492a
LuisGH18 <lguanilo270@gmail.com>
2024-06-20 10:50:19 -0500

    Add: create DetailPurchaseModel

--- api/model/DetailPurchaseModel.py
+++ api/model/DetailPurchaseModel.py
@@ -9,7 +9,8 @@ class DetailPurchase(models.Model):
     quantity = models.IntegerField()
     total = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    purchase_id = models.ForeignKey(Purchase, on_delete=models.CASCADE)
+    
+    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
     
     def save(self, *args, **kwargs):
         self.total = self.price * self.quantity
--- api/serializers/DetailPurchaseSerializer.py
+++ api/serializers/DetailPurchaseSerializer.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
-from api.models import DetailPurchase
+from api.models import DetailPurchase, Purchase
+from api.serializers.PurchaseSerializer import PurchaseSerializer
 
 class DetailPurchaseSerializer(serializers.ModelSerializer):
+    purchase_id = serializers.PrimaryKeyRelatedField(queryset=Purchase.objects.all(), source='purchase', write_only=True)
+    purchase_obj = PurchaseSerializer(source='purchase', read_only=True)
     total = serializers.DecimalField(max_digits=10, decimal_places=2, read_only=True)
 
     class Meta:
         model = DetailPurchase
-        fields = ['id', 'date_purchase', 'item', 'price', 'quantity', 'total', 'purchase_id']
+        fields = ['id', 'date_purchase', 'item', 'price', 'quantity', 'total', 'purchase_id','purchase_obj']
+    
+    def create(self, validated_data):
+        purchase = validated_data.pop('purchase')
+        detail_purchase = DetailPurchase.objects.create(purchase=purchase, **validated_data)
+        return detail_purchase
