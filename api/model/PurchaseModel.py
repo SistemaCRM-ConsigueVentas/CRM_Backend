dca3b25d02429602317b526e85abce0574b30c8a
LuisGH18 <lguanilo270@gmail.com>
2024-06-20 10:30:02 -0500

    Add: create PurchaseModel

--- api/model/PurchaseModel.py
+++ api/model/PurchaseModel.py
@@ -1,5 +1,5 @@
 from django.db import models
-from api.models import Provider, Payment
+from api.models import Provider
 
 class Purchase(models.Model):
     id = models.AutoField(primary_key=True)
@@ -11,7 +11,6 @@ class Purchase(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     
     provider = models.ForeignKey(Provider, on_delete=models.CASCADE)
-    payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
     
     def __str__(self):
         return str(self.id)
--- api/serializers/PurchaseSerializer.py
+++ api/serializers/PurchaseSerializer.py
@@ -1,20 +1,16 @@
 from rest_framework import serializers
-from api.models import Purchase, Provider, Payment
+from api.models import Purchase, Provider
 from api.serializers.ProviderSerializer import ProviderSerializer
-from api.serializers.PaymentSerializer import PaymentSerializer
 
 class PurchaseSerializer(serializers.ModelSerializer):
     provider_id = serializers.PrimaryKeyRelatedField(queryset=Provider.objects.all(), source='provider', write_only=True)
-    payment_id = serializers.PrimaryKeyRelatedField(queryset=Payment.objects.all(), source='payment', write_only=True)
     provider_obj = ProviderSerializer(source='provider', read_only=True)
-    payment_obj = PaymentSerializer(source='payment', read_only=True)
 
     class Meta:
         model = Purchase
-        fields = ['id', 'date_purchase', 'number_bill', 'total', 'estatus', 'created_at', 'updated_at', 'provider_id', 'payment_id', 'provider_obj', 'payment_obj']
+        fields = ['id', 'date_purchase', 'number_bill', 'total', 'estatus', 'created_at', 'updated_at', 'provider_id', 'provider_obj']
 
     def create(self, validated_data):
         provider = validated_data.pop('provider')
-        payment = validated_data.pop('payment')
-        purchase = Purchase.objects.create(provider=provider, payment=payment, **validated_data)
+        purchase = Purchase.objects.create(provider=provider, **validated_data)
         return purchase
