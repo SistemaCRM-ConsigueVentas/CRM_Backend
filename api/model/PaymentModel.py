ed68a4ad5fabae4f262779212e1b2514aaa76144
LuisGH18 <lguanilo270@gmail.com>
2024-06-20 10:40:12 -0500

    Add: create PaymentModel

--- api/model/PaymentModel.py
+++ api/model/PaymentModel.py
@@ -1,14 +1,16 @@
 from django.db import models
+from api.model.PurchaseModel import Purchase
 
 class Payment(models.Model):
     id = models.AutoField(primary_key=True)    
     date_payment = models.DateField()
     date_limit = models.DateField()
-    
     payment_method = models.CharField(max_length=50)
     total = models.DecimalField(max_digits=10, decimal_places=2)
     cancelled_total = models.DecimalField(max_digits=10, decimal_places=2)
     estatus = models.CharField(max_length=50)
+        
+    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.id
--- api/serializers/PaymentSerializer.py
+++ api/serializers/PaymentSerializer.py
@@ -1,7 +1,17 @@
 from api.model.PaymentModel import *
 from rest_framework import serializers
+from api.models import Purchase, Payment
+from api.serializers.PurchaseSerializer import PurchaseSerializer
 
-class PaymentSerializer(serializers.ModelSerializer):
+class PaymentSerializer(serializers.ModelSerializer):    
+    purchase_id = serializers.PrimaryKeyRelatedField(queryset=Purchase.objects.all(), source='purchase', write_only=True)
+    purchase_obj = PurchaseSerializer(source='purchase', read_only=True)
+    
     class Meta:
         model = Payment
-        fields = '__all__'
+        fields = ['id', 'date_payment', 'date_limit', 'payment_method', 'total', 'cancelled_total', 'estatus', 'purchase_id', 'purchase_obj']
+
+    def create(self, validated_data):
+        purchase = validated_data.pop('purchase')
+        payment = Payment.objects.create(purchase=purchase, **validated_data)
+        return payment
