85a5f4a8290b522cf15c3317bfc23efac23ffb66
LuisGH18 <lguanilo270@gmail.com>
2024-07-01 11:38:30 -0500

    Add: field 'description' in DetailPurchase

--- api/model/DetailPurchaseModel.py
+++ api/model/DetailPurchaseModel.py
@@ -8,6 +8,7 @@ class DetailPurchase(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField()
     total = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
+    description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
     purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
