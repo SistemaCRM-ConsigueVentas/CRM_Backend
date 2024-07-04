ec764269384dede756f32ce56c4a9dfb3e1b61d9
LuisGH18 <lguanilo270@gmail.com>
2024-07-04 11:43:32 -0500

    fix: NotificationModel

--- api/model/NotificationModel.py
+++ api/model/NotificationModel.py
@@ -2,12 +2,12 @@ from django.db import models
 from api.model.UserModel import User
 
 class Notification(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.TextField(null=False)
     description = models.TextField(null=False)
     date = models.DateField()
     user_id =models.ForeignKey(User, on_delete=models.CASCADE)
-
+    status = models.IntegerField(default=1)
 
     def __str__(self):
-        return self.pk
-    
+        return self.id
