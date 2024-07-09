f1d8bc6d36f50668b388ae4923edc9e9fed6fd50
LuisGH18 <lguanilo270@gmail.com>
2024-07-09 12:20:45 -0500

    Add: detailpurchase_id in PurchaseModel

--- api/serializers/DetailPurchaseSerializer.py
+++ api/serializers/DetailPurchaseSerializer.py
@@ -1,15 +1,12 @@
 from rest_framework import serializers
-from api.models import DetailPurchase, Purchase
-from api.serializers.PurchaseSerializer import PurchaseSerializer
+from api.models import DetailPurchase
 
 class DetailPurchaseSerializer(serializers.ModelSerializer):
-    purchase_id = serializers.PrimaryKeyRelatedField(queryset=Purchase.objects.all(), source='purchase', write_only=True)
-    purchase_obj = PurchaseSerializer(source='purchase', read_only=True)
     total = serializers.DecimalField(max_digits=10, decimal_places=2, read_only=True)
 
     class Meta:
         model = DetailPurchase
-        fields = ['id', 'date_purchase', 'item', 'price', 'quantity', 'total','description', 'purchase_id','purchase_obj']
+        fields = ['id', 'date_purchase', 'item', 'price', 'quantity', 'total','description']
     
     def create(self, validated_data):
         purchase = validated_data.pop('purchase')
