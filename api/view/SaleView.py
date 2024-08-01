20622e49943f3bbca070009038269ba4d099db12
Harrinson <hfloresbernuy@gmail.com>
2024-07-31 19:05:17 -0500

    ADD: Data in sale

--- api/view/SaleView.py
+++ api/view/SaleView.py
@@ -1,11 +1,13 @@
-from rest_framework import generics
+from rest_framework import generics ,status
 from rest_framework.permissions import IsAuthenticated
 from api.models import *
 from api.serializers.SaleSerializer import *
 import datetime
 from django.http import JsonResponse
 from django.db.models import Sum
+from django.db.models.functions import TruncMonth, TruncYear
 from decimal import Decimal, ROUND_HALF_UP
+from rest_framework.response import Response
 
 #Listar y crear cliente
 class SaleListCreateView(generics.ListCreateAPIView):
@@ -26,6 +28,8 @@ class SalesInRangeView(generics.ListAPIView):
     def get(self, request):
         start_date = request.GET.get('start_date')
         end_date = request.GET.get('end_date')
+        group_by = request.GET.get('type','daily')
+        """ http://localhost:8000/app/sales/range-days/?start_date=2024-04-24&end_date=2024-04-30&type=yearly """
 
         if not start_date or not end_date:
             return JsonResponse({"error": "Please provide both start_date and end_date"}, status=400)
@@ -36,18 +40,61 @@ class SalesInRangeView(generics.ListAPIView):
         except ValueError:
             return JsonResponse({"error": "Invalid date format. Use YYYY-MM-DD"}, status=400)
 
-        sales = Sale.objects.filter(date__range=(start_date, end_date))
+        if group_by == 'daily':
+            response_data = self.get_sales_by_day(start_date, end_date)
+        elif group_by == 'monthly':
+            response_data = self.get_sales_by_month(start_date, end_date)
+        elif group_by == 'yearly':
+            response_data = self.get_sales_by_year(start_date, end_date)
+        else:
+            return Response({"error":"Invalid type parameter. Use 'daily', 'monthly', or 'yearly'"}, status=status.HTTP_404_NOT_FOUND)
 
-        # Agrupar ventas por día y calcular el total por día
+        return Response(response_data)
+
+
+    def get_sales_by_day(self, start_date, end_date):
+        sales = Sale.objects.filter(date__range = (start_date,end_date))
+        print(sales)
         sales_by_day = sales.values('date').annotate(total_amount=Sum('total'))
 
-        # Crear el arreglo de respuesta
         response_data = []
+
         for sale in sales_by_day:
+            total_amount =Decimal(sale['total_amount']).quantize(Decimal('0.01'),rounding=ROUND_HALF_UP)
+            response_data.append(
+                {
+                    'name':sale['date'].strftime('%d-%m'),
+                    'total':str(total_amount)
+                }
+            )
+        return response_data
+    
+    
+    def get_sales_by_month(self, start_date, end_date):
+        sales = Sale.objects.filter(date__range=(start_date, end_date))
+        sales_by_month = sales.annotate(month=TruncMonth('date')).values('month').annotate(total_amount=Sum('total')).order_by('month')
+
+        response_data = []
+        for sale in sales_by_month:
             total_amount = Decimal(sale['total_amount']).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
             response_data.append({
-                'name': sale['date'].strftime('%d-%m'),
+                'name': sale['month'].strftime('%Y-%m'),
                 'total': str(total_amount)
             })
+        
+        return response_data
+    
 
-        return JsonResponse(response_data, safe=False)
+    def get_sales_by_year(self, start_date, end_date):
+        sales = Sale.objects.filter(date__range =(start_date,end_date))
+        sales_by_year = sales.annotate(year=TruncYear('date')).values('year').annotate(total_amount=Sum('total')).order_by('year')
+        response_data = []
+        for sale in sales_by_year:
+            total_amount = Decimal(sale['total_amount']).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
+            response_data.append({
+                'name': sale['year'].strftime('%Y'),
+                'total': str(total_amount)
+            })
+        
+        return response_data
+        
